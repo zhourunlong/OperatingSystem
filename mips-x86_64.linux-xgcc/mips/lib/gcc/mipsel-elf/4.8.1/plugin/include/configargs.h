a2ad6181b4eb48071e8c1e09da1afe9b9de7b882
zhyheyheyhey <zhao_heyang@163.com>
2020-11-17 13:12:52 +0800

    Update mips from cloud.tsinghua.edu.cn

---
+++ mips-x86_64.linux-xgcc/mips/lib/gcc/mipsel-elf/4.8.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.8.1/configure --target=mipsel-elf --prefix=/usr/local/mips --program-prefix=mips- --with-gnu-as --with-gnu-ld --enable-languages=c";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
