37382a660fb5c4b06a92947a347da0766abcd8b5
zhourunlong <zhourunlongvector@gmail.com>
2020-12-09 17:18:59 +0800

    fix compile issue

--- fuse/lfs/main.cpp
+++ fuse/lfs/main.cpp
@@ -1,4 +1,6 @@
 #include "index.h"
+#include "logger.h"
+#include "path.h"
 
 int main(int argc, char** argv) {
     set_log_level(DEBUG);
@@ -7,20 +9,6 @@ int main(int argc, char** argv) {
 
     int ret; 
 	struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
-
-    extern struct options options;
-    
-    options.filename = strdup("hello");
-	options.contents = strdup("Hello World!\n");
-
-    if (fuse_opt_parse(&args, &options, option_spec, NULL) == -1)
-		return 1;
-
-    if (options.show_help) {
-		show_help(argv[0]);
-		assert(fuse_opt_add_arg(&args, "--help") == 0);
-		args.argv[0][0] = '\0';
-	}
 
     ret = fuse_main(args.argc, args.argv, &ops, NULL);
 	fuse_opt_free_args(&args);
