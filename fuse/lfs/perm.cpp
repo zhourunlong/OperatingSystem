e5ecbd42ee8b1a4b7c61e7f5cd51c2074b71ec1f
zhourunlong <zhourunlongvector@gmail.com>
2020-12-10 21:19:28 +0800

    dir timestamp

--- fuse/lfs/perm.cpp
+++ fuse/lfs/perm.cpp
@@ -5,6 +5,8 @@
 #include "utility.h"
 #include "blockio.h"
 
+#include <time.h>
+
 int o_chmod(const char* path, mode_t mode, struct fuse_file_info* fi) {
     if (DEBUG_PRINT_COMMAND)
         logger(DEBUG, "CHMOD, %s, %d, %p\n",
@@ -15,13 +17,14 @@ int o_chmod(const char* path, mode_t mode, struct fuse_file_info* fi) {
     fi->fh = fh;
     if (locate_err != 0) {
         if (ERROR_PERM)
-            logger(ERROR, "[Error] Cannot access the path (error #%d).\n", locate_err);
+            logger(ERROR, "[ERROR] Cannot access the path (error #%d).\n", locate_err);
         return locate_err;
     }
 
     inode block_inode;
     get_inode_from_inum(&block_inode, fh);
     block_inode.permission = mode;
+    clock_gettime(CLOCK_REALTIME, &block_inode.ctime);
     new_inode_block(&block_inode, block_inode.i_number);
 
     return 0;
@@ -45,6 +48,7 @@ int o_chown(const char* path, uid_t uid, gid_t gid, struct fuse_file_info* fi) {
     get_inode_from_inum(&block_inode, fh);
     block_inode.perm_uid = uid;
     block_inode.perm_gid = gid;
+    clock_gettime(CLOCK_REALTIME, &block_inode.ctime);
     new_inode_block(&block_inode, block_inode.i_number);
 
     return 0;
